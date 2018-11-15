1fb08f4a691b28cec025318cd9867b366bbf4afe
Robert Gore <DarkburstGX@gmail.com>
2018-11-15 11:22:41 -0800

    final commit of 2.4 Branching.  Includes finalized Amusement Park assignment and planning document for November 15, 2018 filled out for the morning.

---
+++ fidgetech-daily-planning-111518.md
@@ -0,0 +1,49 @@
+# Daily Planning Doc
+
+##### Date:
+
+## BEFORE BEGINNING WORK:
+
+
+#### Please list the names of the lessons you are going to work on today:
+
+* 2.4 Branching
+
+* 2.4 More Branching (if time)
+
+
+
+#### Please list 3 concepts you are going to focus on today. (For example, css grids, html forms, string methods, etc.):
+
+1. JavaScript / jQuery forms
+
+2. JavaScript Branching
+
+3. CSS based on form input
+  
+
+
+## BEFORE YOU LEAVE:
+
+
+#### Please list the names of the lessons you completed today:
+
+(remove or add more bullet points as needed)
+
+*
+
+*
+
+*
+
+
+
+#### Please list the names of the projects you worked on today:
+
+(remove or add more bullet points as needed)
+
+*
+
+*
+
+*
--- js/scripts3.js
+++ js/scripts3.js
@@ -1,23 +1,73 @@
 var inputToCentimeters = function (heightFeetInput, heightInchesInput) {
   var totalInchesHeight = (heightFeetInput * 12) + heightInchesInput;
   console.log(totalInchesHeight);
-  var totalCentimetersHeight = totalInchesHeight * 2.54;
+  var totalCentimetersHeight = Math.ceil(totalInchesHeight * 2.54);
   console.log(totalCentimetersHeight);
   return parseInt(totalCentimetersHeight);
 }
 
 $(document).ready(function() {
   $("form#riderHeightInfo").submit(function(event) {
-    event.preventDefault();
-
     var heightFeetInput = parseInt($("#heightF").val());
     var heightInchesInput = parseInt($("#heightI").val());
 
     var finalHeight = inputToCentimeters(heightFeetInput, heightInchesInput);
     console.log(finalHeight);
-
-    if (finalheight >= 210) {
-      
+    $("form#riderHeightInfo").hide();
+    if (finalHeight > 225) {
+      $(".tooTall").show();
     }
+    else if (finalHeight >= 215) {
+      $(".rides").show();
+      $(".tall-ride").addClass("green-bg");
+      $(".above-average-ride").addClass("green-bg");
+      $(".average-ride").addClass("blue-bg");
+      $(".below-average-ride").addClass("red-bg");
+      $(".short-ride").addClass("red-bg");
+    }
+    else if (finalHeight >= 190) {
+      $(".rides").show();
+      $(".tall-ride").addClass("green-bg");
+      $(".above-average-ride").addClass("green-bg");
+      $(".average-ride").addClass("blue-bg");
+      $(".below-average-ride").addClass("blue-bg");
+      $(".short-ride").addClass("red-bg");
+    }
+    else if (finalHeight >= 150) {
+      $(".rides").show();
+      $(".tall-ride").addClass("yellow-bg");
+      $(".above-average-ride").addClass("green-bg");
+      $(".average-ride").addClass("green-bg");
+      $(".below-average-ride").addClass("blue-bg");
+      $(".short-ride").addClass("blue-bg");
+    }
+    else if (finalHeight >= 120) {
+      $(".rides").show();
+      $(".tall-ride").addClass("red-bg");
+      $(".above-average-ride").addClass("yellow-bg");
+      $(".average-ride").addClass("yellow-bg");
+      $(".below-average-ride").addClass("green-bg");
+      $(".short-ride").addClass("blue-bg");
+    }
+    else if (finalHeight >= 70) {
+      $(".rides").show();
+      $(".tall-ride").addClass("red-bg");
+      $(".above-average-ride").addClass("red-bg");
+      $(".average-ride").addClass("yellow-bg");
+      $(".below-average-ride").addClass("yellow-bg");
+      $(".short-ride").addClass("green-bg");
+    }
+    else if (finalHeight >= 40) {
+      $(".rides").show();
+      $(".tall-ride").addClass("red-bg");
+      $(".above-average-ride").addClass("red-bg");
+      $(".average-ride").addClass("red-bg");
+      $(".below-average-ride").addClass("yellow-bg");
+      $(".short-ride").addClass("yellow-bg");
+    }
+    else {
+      $(".tooShort").show();
+    }
+    event.preventDefault();
   });
 });
